32becedf8f2272bc110d918f9483a4a9655ef631
zsyzgu <zsyzgu@163.com>
2017-11-06 17:35:36 +0800

    First commit.

---
+++ Assets/Scripts/SourceManager.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class SourceManager : MonoBehaviour {
+    static private SourceManager instance;
+
+    private KinectSensor sensor;
+    private MultiSourceFrameReader reader;
+    private CoordinateMapper mapper;
+
+    private int colorWidth;
+    private int colorHeight;
+    private Texture2D colorTexture;
+    private byte[] colorData;
+
+    private int infraredWidth;
+    private int infraredHeight;
+    private Texture2D infraredTexture;
+    private ushort[] infraredRawData;
+    private byte[] infraredData;
+
+    private int depthWidth;
+    private int depthHeight;
+    private ushort[] depthData;
+    
+    static public CoordinateMapper getCoordinateMapper()
+    {
+        return instance.mapper;
+    }
+
+    static public int getColorWidth()
+    {
+        return instance.colorWidth;
+    }
+
+    static public int getColorHeight()
+    {
+        return instance.colorHeight;
+    }
+
+    static public Texture2D getColorTexture()
+    {
+        return instance.colorTexture;
+    }
+
+    static public int getInfraredWidth()
+    {
+        return instance.infraredWidth;
+    }
+
+    static public int getInfraredHeight()
+    {
+        return instance.infraredHeight;
+    }
+
+    static public Texture2D getInfraredTexture()
+    {
+        return instance.infraredTexture;
+    }
+
+    static public int getDepthWidth()
+    {
+        return instance.depthWidth;
+    }
+
+    static public int getDepthHeight()
+    {
+        return instance.depthHeight;
+    }
+
+    static public ushort[] getDepthData()
+    {
+        return instance.depthData;
+    }
+
+    void Awake()
+    {
+        instance = this;
+
+        sensor = KinectSensor.GetDefault();
+
+        if (sensor != null)
+        {
+            reader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Infrared | FrameSourceTypes.Depth);
+            mapper = sensor.CoordinateMapper;
+
+            var colorFrameDesc = sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Rgba);
+            colorWidth = colorFrameDesc.Width;
+            colorHeight = colorFrameDesc.Height;
+            colorTexture = new Texture2D(colorFrameDesc.Width, colorFrameDesc.Height, TextureFormat.RGBA32, false);
+            colorData = new byte[colorFrameDesc.BytesPerPixel * colorFrameDesc.LengthInPixels];
+
+            var infraredFrameDesc = sensor.InfraredFrameSource.FrameDescription;
+            infraredWidth = infraredFrameDesc.Width;
+            infraredHeight = infraredFrameDesc.Height;
+            infraredTexture = new Texture2D(infraredFrameDesc.Width, infraredFrameDesc.Height, TextureFormat.RGBA32, false);
+            infraredRawData = new ushort[infraredFrameDesc.LengthInPixels];
+            infraredData = new byte[infraredFrameDesc.LengthInPixels * 4];
+
+            var depthFrameDesc = sensor.DepthFrameSource.FrameDescription;
+            depthWidth = depthFrameDesc.Width;
+            depthHeight = depthFrameDesc.Height;
+            depthData = new ushort[depthFrameDesc.LengthInPixels];
+
+            if (!sensor.IsOpen)
+            {
+                sensor.Open();
+            }
+        }
+	}
+	
+    void updateColor(ColorFrame colorFrame)
+    {
+        if (colorFrame != null)
+        {
+            colorFrame.CopyConvertedFrameDataToArray(colorData, ColorImageFormat.Rgba);
+            colorTexture.LoadRawTextureData(colorData);
+            colorTexture.Apply();
+
+            colorFrame.Dispose();
+            colorFrame = null;
+        }
+    }
+
+    void updateInfrared(InfraredFrame infraredFrame)
+    {
+        if (infraredFrame != null)
+        {
+            infraredFrame.CopyFrameDataToArray(infraredRawData);
+
+            int index = 0;
+            foreach(var ir in infraredRawData)
+            {
+                byte intensity = (byte)(ir >> 8);
+                infraredData[index++] = intensity;
+                infraredData[index++] = intensity;
+                infraredData[index++] = intensity;
+                infraredData[index++] = 255;
+            }
+
+            infraredTexture.LoadRawTextureData(infraredData);
+            infraredTexture.Apply();
+
+            infraredFrame.Dispose();
+            infraredFrame = null;
+        }
+    }
+
+    void updateDepth(DepthFrame depthFrame)
+    {
+        if (depthFrame != null)
+        {
+            depthFrame.CopyFrameDataToArray(depthData);
+
+            depthFrame.Dispose();
+            depthFrame = null;
+        }
+    }
+
+	void Update () {
+		if (reader != null)
+        {
+            var frame = reader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                updateColor(frame.ColorFrameReference.AcquireFrame());
+                updateInfrared(frame.InfraredFrameReference.AcquireFrame());
+                updateDepth(frame.DepthFrameReference.AcquireFrame());
+            }
+        }
+	}
+
+    void OnApplicationQuit()
+    {
+        if (reader != null)
+        {
+            reader.Dispose();
+            reader = null;
+        }
+
+        if (sensor != null)
+        {
+            if (sensor.IsOpen)
+            {
+                sensor.Close();
+            }
+            sensor = null;
+        }
+    }
+}
---
+++ Assets/Scripts/View3D.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class View3D : MonoBehaviour
+{
+    private const int DOWNSAMPLE = 2;
+    private const float DEPTH_SCALE = 0.1f;
+
+    private CoordinateMapper mapper;
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Vector2[] uv;
+    private int[] triangles;
+
+	void Start () {
+        mapper = SourceManager.getCoordinateMapper();
+        createMesh();
+	}
+	
+	void Update () {
+        updateMesh();
+	}
+
+    void createMesh()
+    {
+        int width = SourceManager.getDepthWidth() / DOWNSAMPLE;
+        int height = SourceManager.getDepthHeight() / DOWNSAMPLE;
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+
+        vertices = new Vector3[width * height];
+        uv = new Vector2[width * height];
+        triangles = new int[6 * (width - 1) * (height - 1)];
+
+        int triangleIndex = 0;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = (y * width) + x;
+
+                vertices[index] = new Vector3(x - width / 2, -(y - height / 2), 100);
+                uv[index] = new Vector2((float)x / width, (float)y / height);
+
+                if (x != (width - 1) && y != (height - 1))
+                {
+                    int topLeft = index;
+                    int topRight = index + 1;
+                    int bottomLeft = index + width;
+                    int bottomRight = index + width + 1;
+
+                    triangles[triangleIndex++] = topLeft;
+                    triangles[triangleIndex++] = topRight;
+                    triangles[triangleIndex++] = bottomLeft;
+                    triangles[triangleIndex++] = bottomLeft;
+                    triangles[triangleIndex++] = topRight;
+                    triangles[triangleIndex++] = bottomRight;
+                }
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    void updateMesh()
+    {
+        GetComponent<Renderer>().material.mainTexture = SourceManager.getColorTexture();
+
+        ushort[] depthData = SourceManager.getDepthData();
+        int depthWidth = SourceManager.getDepthWidth();
+        int depthHeight = SourceManager.getDepthHeight();
+        int colorWidth = SourceManager.getColorWidth();
+        int colorHeight = SourceManager.getColorHeight();
+
+        ColorSpacePoint[] colorSpace = new ColorSpacePoint[depthData.Length];
+        mapper.MapDepthFrameToColorSpace(depthData, colorSpace);
+
+        for (int y = 0; y < depthHeight; y += DOWNSAMPLE)
+        {
+            for (int x = 0; x < depthWidth; x += DOWNSAMPLE)
+            {
+                int rawIndex = y * depthWidth + x;
+                int indexX = x / DOWNSAMPLE;
+                int indexY = y / DOWNSAMPLE;
+                int downIndex = indexY * (depthWidth / DOWNSAMPLE) + indexX;
+
+                float avgDepth = getAvgDepth(depthData, x, y, depthWidth, depthHeight) * DEPTH_SCALE;
+                vertices[downIndex].z = avgDepth;
+
+                if (depthData[rawIndex] == 0)
+                {
+                    vertices[downIndex].z = 500;
+                } else
+                {
+                    vertices[downIndex].z = depthData[rawIndex] * DEPTH_SCALE;
+                }
+
+                ColorSpacePoint colorSpacePoint = colorSpace[rawIndex];
+                uv[downIndex] = new Vector2(colorSpacePoint.X / colorWidth, colorSpacePoint.Y / colorHeight);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    float getAvgDepth(ushort[] depthData, int xBase, int yBase, int width, int height)
+    {
+        float sum = 0;
+
+        for (int y = yBase; y < yBase + DOWNSAMPLE; y++)
+        {
+            for (int x = xBase; x < xBase + DOWNSAMPLE; x++)
+            {
+                int index = y * width + x;
+
+                if (depthData[index] == 0)
+                {
+                    sum += 5000;
+                } else
+                {
+                    sum += depthData[index];
+                }
+            }
+        }
+
+        return sum / (DOWNSAMPLE * DOWNSAMPLE);
+    }
+}
