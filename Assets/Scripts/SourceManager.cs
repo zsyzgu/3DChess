8495a0daeb41f6ffd763413548d46396bec2f89c
zsyzgu <zsyzgu@163.com>
2017-11-08 20:55:26 +0800

    Depth Data Filter.

--- Assets/Scripts/SourceManager.cs
+++ Assets/Scripts/SourceManager.cs
@@ -5,6 +5,10 @@ using Windows.Kinect;
 using System.Threading.Tasks;
 
 public class SourceManager : MonoBehaviour {
+    private const int DEPTH_QUEUE_LENGTH = 8;
+
+    public bool openFilter = true;
+
     static private SourceManager instance;
 
     private KinectSensor sensor;
@@ -27,6 +31,7 @@ public class SourceManager : MonoBehaviour {
     private int depthWidth;
     private int depthHeight;
     private ushort[] depthData;
+    private Queue<ushort[]> depthQueue;
     
     static public CoordinateMapper getCoordinateMapper()
     {
@@ -108,6 +113,7 @@ public class SourceManager : MonoBehaviour {
             depthWidth = depthFrameDesc.Width;
             depthHeight = depthFrameDesc.Height;
             depthData = new ushort[depthFrameDesc.LengthInPixels];
+            depthQueue = new Queue<ushort[]>();
 
             if (!sensor.IsOpen)
             {
@@ -156,11 +162,129 @@ public class SourceManager : MonoBehaviour {
         }
     }
 
+    ushort[] depthDataPixelFiltering(ushort[] rawDepthData)
+    {
+        const int INNER_THRESHOLD = 2;
+        const int OUTER_THRESHOLD = 4;
+
+        ushort[] depthData = new ushort[rawDepthData.Length];
+
+        Parallel.For(0, depthHeight, y =>
+        {
+            for (int x = 0; x < depthWidth; x++)
+            {
+                int index = y * depthWidth + x;
+                if (rawDepthData[index] == 0)
+                {
+                    int innerBand = 0;
+                    int outerBand = 0;
+                    int sum = 0;
+                    for (int dx = -2; dx <= 2; dx++)
+                    {
+                        for (int dy = -2; dy <= 2; dy++)
+                        {
+                            if (dx != 0 || dy != 0)
+                            {
+                                int xSearch = x + dx;
+                                int ySearch = y + dy;
+
+                                if (0 <= xSearch && xSearch < depthWidth && 0 <= ySearch && ySearch < depthHeight)
+                                {
+                                    int searchIndex = ySearch * depthWidth + xSearch;
+                                    if (rawDepthData[searchIndex] != 0)
+                                    {
+                                        sum += rawDepthData[searchIndex];
+                                        if (-1 <= dx && dx <= 1 && -1 <= dy && dy <= 1)
+                                        {
+                                            innerBand++;
+                                        } else
+                                        {
+                                            outerBand++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (innerBand >= INNER_THRESHOLD || outerBand >= OUTER_THRESHOLD)
+                    {
+                        depthData[index] = (ushort)(sum / (innerBand + outerBand));
+                    } else
+                    {
+                        depthData[index] = 0;
+                    }
+                } else
+                {
+                    depthData[index] = rawDepthData[index];
+                }
+            }
+        });
+
+        return depthData;
+    }
+
+    ushort[] depthDataMovingAverage(ushort[] rawDepthData)
+    {
+        const int DEPTH_CHANGE_THRESHOLD = 10;
+
+        ushort[] depthData = new ushort[rawDepthData.Length];
+
+        depthQueue.Enqueue(rawDepthData);
+        if (depthQueue.Count > DEPTH_QUEUE_LENGTH)
+        {
+            depthQueue.Dequeue();
+        }
+
+        Parallel.For(0, depthHeight, y =>
+        {
+            for (int x = 0; x < depthWidth; x++)
+            {
+                int index = y * depthWidth + x;
+                int sum = 0;
+                int cnt = 0;
+                foreach (ushort[] item in depthQueue)
+                {
+                    if (item[index] != 0)
+                    {
+                        sum += item[index];
+                        cnt++;
+                    }
+                }
+                if (cnt == 0)
+                {
+                    depthData[index] = 0;
+                } else
+                {
+                    depthData[index] = (ushort)(sum / cnt);
+                    if (Mathf.Abs(rawDepthData[index] - depthData[index]) > DEPTH_CHANGE_THRESHOLD)
+                    {
+                        depthData[index] = rawDepthData[index];
+                        foreach (ushort[] item in depthQueue)
+                        {
+                            item[index] = 0;
+                        }
+                    }
+                }
+            }
+        });
+
+        return depthData;
+    }
+
     void updateDepth(DepthFrame depthFrame)
     {
         if (depthFrame != null)
         {
             depthFrame.CopyFrameDataToArray(depthData);
+
+            if (openFilter)
+            {
+                depthData = depthDataPixelFiltering(depthData);
+                depthData = depthDataMovingAverage(depthData);
+            } else
+            {
+                depthQueue.Clear();
+            }
 
             depthFrame.Dispose();
             depthFrame = null;
